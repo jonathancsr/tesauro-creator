124d8002c201012361cec69d34092e0408fc98ae
jonathan.candido <jonathan.candido@toroinvestimentos.com.br>
2021-05-09 20:25:49 -0300

    feat: ajust app and index root archives

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import React,{useEffect} from "react";
+import React from "react";
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import {getArticlesWithQueryAndDate} from './service/getArticles'
-
+import Routes from "./routes";
 
 function App() {
-  useEffect(() => {
-    getArticlesWithQueryAndDate('ICSE',2018,2021).then(res => console.log(res.data))
-  },[])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes/>
+    </Router>
   );
 }
 
